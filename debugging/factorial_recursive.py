8be9428b2344a018ba0b785ad34861323eae0cb7
PoussPouss <guillaume_font@orange.fr>
2025-05-05 11:18:22 +0200

    Ajout des fichiers checkbook.py, factorial_recursive.py et tic.py avec des fonctionnalités de gestion de compte, calcul de factoriel et jeu de morpion.

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if len(sys.argv) < 2:
+    print("Usage: ./factorial_recursive.py <number>")
+    sys.exit(1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
